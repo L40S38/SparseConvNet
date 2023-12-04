a88e40c40fa69fec1c122e8035183b970c049272
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2023-12-04 11:46:36 +0900

    install.sh modified

--- install.sh
+++ install.sh
@@ -7,8 +7,9 @@ conda create --name sparseconvnet python=3.6 -y
 conda activate sparseconvnet
 
 #-> 2. install related library
-pip install -U pip
-pip install torch==1.3.0
+#pip install -U pip
+#pip install torch==1.3.0
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install scikit-learn -y
 conda install tqdm -c conda-forge -y
 conda install pillow -c conda-forge -y
