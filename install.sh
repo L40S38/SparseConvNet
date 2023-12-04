dc5ba576516a4095afd6b923129859f645614f3e
L40S38 <shiozawa.l.aa@m.titech.ac.jp>
2023-12-04 19:42:12 +0900

    install.sh modified

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 #-> 1. create an conda environment
 source ~/anaconda3/etc/profile.d/conda.sh
 conda remove --name sparseconvnet --all -y
-conda create --name sparseconvnet python=3.6 -y
+conda create --name sparseconvnet python=3.3 -y
 conda activate sparseconvnet
 
 #-> 2. install related library
-#pip install -U pip
-#pip install torch==1.3.0 torchvision torchaudio
-conda install pytorch=1.3.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch -c nvidia -y
-conda install scikit-learn -y
-conda install tqdm -c conda-forge -y
-conda install pillow -c conda-forge -y
-conda install scipy -c anaconda -y
+pip install -U pip
+pip install torch==1.3.0 torchvision torchaudio
+#conda install pytorch=1.3.0 torchvision torchaudio cudatoolkit=10.1 -c pytorch -c nvidia -y
+#conda install scikit-learn -y
+#conda install tqdm -c conda-forge -y
+#conda install pillow -c conda-forge -y
+#conda install scipy -c anaconda -y
 
 #-> 3. run setup
 rm -rf build/ dist/ sparseconvnet.egg-info sparseconvnet/SCN*.so
